3272453377658d2db7a7e4fce71cf3700867fd5e
reggae-krk <monika.gorecka.agh@gmail.com>
2018-09-06 21:59:28 +0200

    Implement DoublyNode class

--- src/main/java/DoublyLinkedList/DoublyNode.java
+++ src/main/java/DoublyLinkedList/DoublyNode.java
@@ -1,4 +1,36 @@
 package DoublyLinkedList;
 
-public class DoublyNode {
+public class DoublyNode<T> {
+
+    private DoublyNode<T> next;
+    private DoublyNode<T> previous;
+    private T data;
+
+    public DoublyNode(T data) {
+        this.data = data;
+    }
+
+    public DoublyNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(DoublyNode<T> next) {
+        this.next = next;
+    }
+
+    public DoublyNode<T> getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(DoublyNode<T> previous) {
+        this.previous = previous;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 }
