0b3b3a5a33ec83439882d0382e21a7d07e77e2b3
reggae-krk <monika.gorecka.agh@gmail.com>
2018-09-06 22:49:27 +0200

    Create init method

--- src/test/java/DoublyLinkedList/DoublyLinkedListTest.java
+++ src/test/java/DoublyLinkedList/DoublyLinkedListTest.java
@@ -4,4 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DoublyLinkedListTest {
 
+    private DoublyLinkedList<String> init() {
+        DoublyLinkedList<String> list = new DoublyLinkedList<String>();
+        list.add("test0");
+        list.add("test1");
+        list.add("test2");
+        return list;
+    }
+
 }
