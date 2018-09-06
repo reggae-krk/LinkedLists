336590fa48b335ba8ceef3636a2ea532eae6d037
reggae-krk <monika.gorecka.agh@gmail.com>
2018-09-06 21:08:15 +0200

    Create testAdd method

--- src/test/java/SinglyLinkedList/SinglyLinkedListTest.java
+++ src/test/java/SinglyLinkedList/SinglyLinkedListTest.java
@@ -1,7 +1,17 @@
 package SinglyLinkedList;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SinglyLinkedListTest {
+
+    @Test
+    void testAdd() {
+        SinglyLinkedList<String> list = new SinglyLinkedList<String>();
+        list.add("test");
+
+        assertEquals("test", list.get(0));
+    }
 
 }
