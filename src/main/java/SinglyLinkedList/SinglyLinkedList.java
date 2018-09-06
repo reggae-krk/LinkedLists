fa89217f5f50c5061be00fb77f701d14ce216a2a
reggae-krk <monika.gorecka.agh@gmail.com>
2018-09-06 19:25:05 +0200

    Create insert method and change argument name in add method

--- src/main/java/SinglyLinkedList/SinglyLinkedList.java
+++ src/main/java/SinglyLinkedList/SinglyLinkedList.java
@@ -7,8 +7,8 @@ public class SinglyLinkedList<T> {
     SinglyNode tail;
     int length;
 
-    public void add(T element) {
-        SinglyNode<T> node = new SinglyNode<T>(element);
+    public void add(T data) {
+        SinglyNode<T> node = new SinglyNode<T>(data);
 
         if (head == null) {
             this.head = node;
@@ -33,6 +33,23 @@ public class SinglyLinkedList<T> {
                 tempNode = tempNode.getNext();
             }
             tempNode.setNext(tempNode.getNext().getNext());
+        }
+    }
+
+    public void insert(int index, T data) {
+        SinglyNode<T> node = new SinglyNode<T>(data);
+        SinglyNode<T> tempNode = head;
+
+        if (index == 0) {
+            node.setNext(head);
+            head = node;
+        }
+        else {
+            for (int i = 1; i < index; i++) {
+                tempNode = tempNode.getNext();
+            }
+            node.setNext(tempNode.getNext());
+            tempNode.setNext(node);
         }
     }
 }
