bed15cb99186f4786a7008c2aaba335d5adaf6c6
reggae-krk <monika.gorecka.agh@gmail.com>
2018-09-06 15:59:53 +0200

    Implement SinglyNode class and add it to proper package

---
+++ src/main/java/SinglyLinkedList/SinglyNode.java
@@ -0,0 +1,27 @@
+package SinglyLinkedList;
+
+public class SinglyNode<T> {
+
+    private SinglyNode<T> next;
+    private T data;
+
+    public SinglyNode(T data) {
+        this.data = data;
+    }
+
+    public SinglyNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(SinglyNode<T> next) {
+        this.next = next;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
