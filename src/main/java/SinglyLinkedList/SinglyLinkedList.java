df1a0fef0c8d5cb39e8ebc458898fbba3815f6a3
reggae-krk <monika.gorecka.agh@gmail.com>
2018-09-06 16:29:27 +0200

    Create add method

--- src/main/java/SinglyLinkedList/SinglyLinkedList.java
+++ src/main/java/SinglyLinkedList/SinglyLinkedList.java
@@ -1,4 +1,24 @@
 package SinglyLinkedList;
 
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
+
+    SinglyNode head;
+    SinglyNode last;
+    SinglyNode tail;
+    int length;
+
+    public void add(T element) {
+        SinglyNode<T> node = new SinglyNode<T>(element);
+
+        if (head == null) {
+            this.head = node;
+        }
+        else {
+            SinglyNode<T> tempNode = head;
+            while (tempNode.getNext() != null) {
+                tempNode = tempNode.getNext();
+            }
+            tempNode.setNext(node);
+        }
+    }
 }
