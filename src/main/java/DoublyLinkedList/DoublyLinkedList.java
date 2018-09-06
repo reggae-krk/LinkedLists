7b3fdfcbc980fc3b7f0f095b0378b00f7cadc64c
reggae-krk <monika.gorecka.agh@gmail.com>
2018-09-06 22:12:38 +0200

    Create add method

--- src/main/java/DoublyLinkedList/DoublyLinkedList.java
+++ src/main/java/DoublyLinkedList/DoublyLinkedList.java
@@ -1,4 +1,27 @@
 package DoublyLinkedList;
 
-public class DoublyLinkedList {
+public class DoublyLinkedList<T> {
+
+    DoublyNode head;
+    DoublyNode last;
+    DoublyLinkedList tail;
+    int size = 0;
+
+    public void add(T data) {
+        DoublyNode<T> node = new DoublyNode<T>(data);
+
+        if (head == null) {
+            this.head = node;
+            head.setPrevious(null);
+        }
+        else {
+            DoublyNode<T> tempNode = head;
+            while (tempNode.getNext() != null) {
+                tempNode = tempNode.getNext();
+            }
+            tempNode.setNext(node);
+            node.setPrevious(tempNode);
+        }
+        size++;
+    }
 }
